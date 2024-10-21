d61846fa0d1e57e37421537c2b9b2d39905555bc
Gerald1818 <pizarrojohngerald46@gmail.com>
2024-10-21 08:51:26 +0800

    remove

--- src/App.js
+++ src/App.js
@@ -16,14 +16,12 @@ function App() {
         <Container>
           <Navbar.Brand as={Link} to="/">My Profile</Navbar.Brand>
           <Navbar.Toggle aria-controls="basic-navbar-nav" />
-          <Navbar.Collapse id="basic-navbar-nav">
             <Nav className="tab">
               <Nav.Link as={Link} to="/">Home</Nav.Link>
               <Nav.Link as={Link} to="/about">About</Nav.Link>
               <Nav.Link as={Link} to="/skills">Skills</Nav.Link>
               <Nav.Link as={Link} to="/contact">Contact</Nav.Link>
             </Nav>
-          </Navbar.Collapse>
         </Container>
       </Navbar>
 
